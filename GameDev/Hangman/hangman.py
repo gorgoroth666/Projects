260e29525a85ac593404c930e23dacc5604ffd52
Cedric Baudry <mr.cedric.baudry@gmail.com>
2015-03-22 21:11:06 +0100

    Hangman game update
    Added a dictionary of words for the hangman game.
    Added a function that picks a word with the required number of letters.

--- GameDev/Hangman/hangman.py
+++ GameDev/Hangman/hangman.py
@@ -1,15 +1,31 @@
 import random
 
-words = ["banana", "architect", "soviet"]
+def pick_word(a, b):
+    """ 
+    Picks a word from a file.
+    Makes sure the length of the word is between a and b inclusive.
+    """
+     
+    word_file = open("10000.words.txt", 'r')
+    word_list = []
+    for line in word_file:
+        line = line.strip("\n")
+        word_list.append(line)
+    word_file.close()
+    
+    while True:
+        w = random.choice(word_list)
+        if len(w) <= b and len(w) >= a:
+            break
+    return w
+    
+       
+secretWord = pick_word(5, 10)
 
-secretWord = words[random.randint(0, 2)]
 isWon = False
 nFailTries = 0
-# two sets:
-# discovered and remaining
-remaining = set(secretWord)
-discovered = set()
 
+remaining = set(secretWord.lower())
 
 while not isWon:
     
@@ -19,8 +35,11 @@ while not isWon:
         else:
             print(c, end="")
     print("")
-        
-    letter = input("Choose a letter")[0]
+
+    while True:
+        letter = input("Choose a letter: ")[0].lower()
+        if letter.isalpha():
+            break
     
     if letter in remaining:       
         remaining.remove(letter)
@@ -28,10 +47,8 @@ while not isWon:
             print("Congratulations, you discovered", secretWord, "with only", nFailTries, "fails")
             isWon = True
             break
-        
-        
-        continue
-        
+                
+        continue        
         
     nFailTries += 1   
     if nFailTries >= 9:
